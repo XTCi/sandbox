97bb0e1ce7649d1eaa97ee252e5450a4a176c83e
ctx <xutc2002@gmail.com>
2026-02-24 09:11:57 +0000

    feat:Added supervisor interface service

--- app/interfaces/service_dependencies.py
+++ app/interfaces/service_dependencies.py
@@ -1,7 +1,9 @@
 from functools import lru_cache
 
+
 from app.services.file import FileService
 from app.services.shell import ShellService
+from app.services.supervisor import SupervisorService
 @lru_cache
 def get_shell_service() -> ShellService:
     return ShellService()
@@ -9,3 +11,7 @@ def get_shell_service() -> ShellService:
 @lru_cache
 def get_file_service() -> FileService:
     return FileService
+
+@lru_cache
+def get_supervisor_service() -> SupervisorService:
+    return SupervisorService
